a1a1c65b6980ea0b58802fee3bcc8c9b052bfd2d
Nathaniel <nfcopier@gmail.com>
2022-06-06 02:26:09 -0600

    #patch Forgot AsyncUnit

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import RxValue from "./rx-value";
 import RxList from "./rx-list";
 import Val from "./val";
+import {AsyncUnit} from "./async-unit";
 
-export {RxValue, RxList, Val};
+export {RxValue, RxList, Val, AsyncUnit};
